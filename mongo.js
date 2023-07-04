f1861ff477f010d263d75260538194208bb7a976
Antti-Jussi Ikonen <ikonenaj@gmail.com>
2023-07-04 10:28:08 +0300

    Tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+  }
+  
+  const password = process.argv[2];
+  
+  const url =
+  `mongodb+srv://ikonenaj:${password}@cluster0.boyldzn.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const addPerson = (name, number) => {
+    const person = new Person({
+        name: name,
+        number: number
+    });
+
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`);
+        mongoose.connection.close();
+    });
+};
+
+const listPersons = () => {
+    console.log('Phonebook:');
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number);
+        })
+    mongoose.connection.close();
+    })
+}
+
+if (process.argv.length === 5) { addPerson(process.argv[3], process.argv[4]) }
+
+if (process.argv.length === 3) { listPersons() }
